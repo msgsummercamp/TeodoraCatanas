c2a685e4a734d7263768a2fcc19b3955c74d8458
Teodora Catanas <126598875+titoakapiticutzudegradina@users.noreply.github.com>
2025-07-16 13:05:23 +0300

    maven chapter

---
+++ maven-archtype-simple/src/main/java/org/apache/maven/archetypes/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.archetypes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main(String[] args) {
+        System.out.println("Environment: " + System.getProperty("env"));
+    }
+
+}
