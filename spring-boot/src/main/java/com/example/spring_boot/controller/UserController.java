dd744bc5cd44e9fd4f17ab2e1f7ce1152e4bc13e
Teodora Catanas <126598875+titoakapiticutzudegradina@users.noreply.github.com>
2025-07-17 13:15:31 +0300

    logs added

--- spring-boot/src/main/java/com/example/spring_boot/controller/UserController.java
+++ spring-boot/src/main/java/com/example/spring_boot/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.spring_boot.controller;
 
 import com.example.spring_boot.model.User;
 import com.example.spring_boot.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -18,19 +21,24 @@ public class UserController {
 
     @GetMapping
     public List<User> getUsers() {
+        log.info("Getting all users");
         return userService.getUsers();
     }
     @GetMapping("/{id}")
     public User getUserById(@PathVariable int id) {
+        log.info("Getting user with id: {}", id);
         return userService.getUserById(id);
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<User> deleteUserById(@PathVariable int id) {
+        log.info("Attempting to deleting user with id: {}", id);
         User deletedUser = userService.getUserById(id);
         boolean isDeleted = userService.deleteUser(id);
         if (isDeleted) {
+            log.info("Deleted user with id: {}", id);
             return ResponseEntity.ok(deletedUser);
         } else {
+            log.warn("User with id: {} not found", id);
             return ResponseEntity.notFound().build();
         }
     }
