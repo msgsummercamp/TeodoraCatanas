6bb92329a9704c13a519efbb52b6dbc3671fd405
Teodora Catanas <126598875+titoakapiticutzudegradina@users.noreply.github.com>
2025-07-21 00:15:32 +0300

    documentation Swagger

--- rest/src/main/java/com/example/rest/controller/UserController.java
+++ rest/src/main/java/com/example/rest/controller/UserController.java
@@ -3,6 +3,9 @@ package com.example.rest.controller;
 import com.example.rest.dto.UserPatchDto;
 import com.example.rest.model.User;
 import com.example.rest.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +28,11 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
+    @Operation(summary = "Get all Users")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved users"),
+            @ApiResponse(responseCode = "204", description = "No users found")
+    })
     public ResponseEntity<Page<User>> getUsers() {
         log.info("Fetching all users");
         Pageable pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
@@ -33,6 +41,11 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get User by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved user"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<User> getUserById(@PathVariable int id) {
         log.info("Fetching user with id: {}", id);
         return Optional.ofNullable(userService.findUserById(id))
@@ -41,6 +54,11 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "Add a new User")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User successfully created"),
+            @ApiResponse(responseCode = "400", description = "Invalid user data")
+    })
     public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
         log.info("Adding new user: {}", user);
         User savedUser = userService.saveUser(user);
@@ -48,6 +66,11 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete User by ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<User> deleteUserById(@PathVariable int id) {
         log.info("Attempting to delete user with id: {}", id);
         User deletedUser = userService.deleteUser(id);
@@ -59,6 +82,11 @@ public class UserController {
     }
 
     @PutMapping
+    @Operation(summary = "Update User")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User successfully updated"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<User> updateUser(@Valid @RequestBody User user) {
         log.info("Updating user: {}", user);
         User updatedUser = userService.updateUser(user);
@@ -70,6 +98,11 @@ public class UserController {
     }
 
     @GetMapping("/username/{username}")
+    @Operation(summary = "Find User by Username")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<Optional<User>> findUserByUsername(@PathVariable String username) {
         log.info("Fetching users with name: {}", username);
         Optional<User> user = userService.findUserByUsername(username);
@@ -81,6 +114,11 @@ public class UserController {
     }
 
     @GetMapping("/email/{email}")
+    @Operation(summary = "Find User by Email")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public ResponseEntity<Optional<User>> findUserByEmail(@PathVariable String email) {
         log.info("Fetching users with email: {}", email);
         Optional<User> user = userService.findUserByEmail(email);
@@ -92,6 +130,11 @@ public class UserController {
     }
 
     @GetMapping("/count")
+    @Operation(summary = "Count all Users")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Total users count retrieved"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Long> countUsers() {
         log.info("Counting all users");
         long count = userService.countUsers();
@@ -100,6 +143,12 @@ public class UserController {
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Partially update User")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User successfully updated"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid patch data")
+    })
     public ResponseEntity<User> patchUser(@PathVariable int id,@Valid @RequestBody UserPatchDto patchDto) {
         log.info("Partially updating user with id: {}", id);
         User updatedUser = userService.patchUser(id, patchDto);
