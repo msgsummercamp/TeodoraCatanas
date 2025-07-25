696e4dd12622ea19573bef66be209ea93cdc7810
Teodora Catanas <126598875+titoakapiticutzudegradina@users.noreply.github.com>
2025-07-25 09:46:06 +0300

    auth service + interceptor, pipe to mask the password, authGuard to protect the home page and confirm-exitGuard on notFound page just to see how it works

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './notFound_component/not-found.component/not-found.component.component';
 import { HomeComponent } from './home/home.component';
+import { authGuard } from './guards/auth.guard';
+import { confirmExitGuard } from './guards/confirm-exit.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then((m) => m.LoginModule),
   },
-  { path: '**', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent, canDeactivate: [confirmExitGuard] },
 ];
